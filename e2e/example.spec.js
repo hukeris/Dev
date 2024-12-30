c7bd4173718f5e32b6f95511fcc09ab7c7f4445b
hukeris <szabyk.reg@gmail.com>
2024-12-30 16:01:02 +0100

    Próba1 - test('has title2', async ({ browser }) =>

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+const { chromium } = require('playwright');
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -12,7 +13,7 @@ test('has title', async ({ page }) => {
   console.log("Hello World!");  
 });
 
-test('get started link', async ({ page }) => {
+/*test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
@@ -20,4 +21,25 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});*/
+
+test('has title2', async ({ browser }) => {
+
+  const context = await browser.newContext({
+    recordVideo: {
+       dir: '.'
+   }
+ });
+
+  const page2 = await context.newPage();
+  await page2.goto('https://www.wikipedia.org/');
+  await page2.getByRole('link', { name: 'English 6,924,000+ articles' }).click();  
+  await page2.screenshot({path: './wiki_screen.png'});
+
+  console.log('browser.browserType(): ' + browser.browserType());
+  console.log("URL: " + page2.url()); 
+
+  await page2.close();
+  await context.close();
+  await browser.close();
 });
